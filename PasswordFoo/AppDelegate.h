99bef4bc3fa3e78b8c7f85eae0feff5ed6422eae
Wayne O. Cochran <wcochran@acm.org>
2014-06-16 16:48:29 -0700

    Initial Commit

---
+++ PasswordFoo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PasswordFoo
+//
+//  Created by Wayne Cochran on 6/16/14.
+//  Copyright (c) 2014 Wayne Cochran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
