8956d9b49bf37307b8ba1bca40e391df2b7644f8
Wayne O. Cochran <wcochran@vancouver.wsu.edu>
2014-06-17 13:33:07 -0700

    Added some comments.

--- PasswordFoo/WOCKeychainWrapper.h
+++ PasswordFoo/WOCKeychainWrapper.h
@@ -10,7 +10,16 @@
 // This is dumbfoundingly simple wrapper for storing a single password.
 // Note that once a password has been stored in the default keychain
 // it persists even after the app and its data have been deleted.
+// The first time an app is run, you may want to 'forceDeletePassword' to make
+// sure there is no hangover password in the keychain (from a previous
+// installation).
+// Also, setting a password will silently fail if a password is already set, so
+// you need to track (in some persistent storage) some indicator that a password
+// is already set to avoid this situation.
 //
+// Note that use should not store plain text passwords at all; e.g., use CC_SHA256
+// (from ) to hash the plaintext passowrd before storing it.
+// (see http://www.raywenderlich.com/6475/basic-security-in-ios-5-tutorial-part-1).
 //
 
 
